8a5ba1f570fbe953a96f71ac61d16831f164ca63
divyanjali2 <navodyadivyanjali2@gmail.com>
2026-02-09 12:24:59 +0530

    logo added

--- resources/js/Layouts/GuestLayout.jsx
+++ resources/js/Layouts/GuestLayout.jsx
@@ -4,13 +4,16 @@ import { Link } from '@inertiajs/react';
 export default function GuestLayout({ children }) {
     return (
         <div className="flex min-h-screen flex-col items-center bg-gray-100 pt-6 sm:justify-center sm:pt-0">
-            <div>
-                <Link href="/">
-                    <ApplicationLogo className="h-20 w-20 fill-current text-gray-500" />
-                </Link>
-            </div>
 
-            <div className="mt-6 w-full overflow-hidden bg-white px-6 py-4 shadow-md sm:max-w-md sm:rounded-lg">
+            {/* Image */}
+            <img
+                src="/images/logo.webp"
+                alt="Login Illustration"
+                className="mb-2 w-48"
+            />
+
+            {/* Card */}
+            <div className="mt-2 w-full overflow-hidden bg-white px-6 py-4 shadow-md sm:max-w-md sm:rounded-lg">
                 {children}
             </div>
         </div>
