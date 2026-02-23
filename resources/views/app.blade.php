fb5e3503f16f348496db57cb05f23e608bce412f
divyanjali2 <navodyadivyanjali2@gmail.com>
2026-02-23 14:45:01 +0530

    added

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,12 +3,8 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
         <title inertia>{{ config('app.name', 'Laravel') }}</title>
-
-        <!-- Fonts -->
-        <!-- <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" /> -->
+        <link rel="icon" type="image/png" href="favicon.png">
         <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@600;700&display=swap" rel="stylesheet">
 
         <!-- Scripts -->
